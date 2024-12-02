8558020a808ad22b372e2a9545e917f413efff90
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-12-01 22:27:04 -0600

    feat: added process spawning

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,59 @@
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+    window::WindowResolution,
+};
+use bevy_prototype_lyon::prelude::*;
+
+mod process;
 
 fn main() {
-    println!("Hello, world!");
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Oversimplified Computer Simulator".to_owned(),
-                resolution: WindowResolution::new(1152.0, 720.0),
+    let mut app = App::new();
+
+    let log_level = if cfg!(debug_assertions) {
+        Level::DEBUG
+    } else {
+        Level::INFO
+    };
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Oversimplified Computer Simulator".to_owned(),
+                    resolution: WindowResolution::new(1152.0, 720.0),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .set(LogPlugin {
+                level: log_level,
                 ..Default::default()
             }),
-            ..Default::default()
-        }))
-        .add_systems(Startup, setup_app)
-        .run();
+    )
+    .add_plugins(ShapePlugin)
+    .add_plugins(process::ProcessPlugin)
+    .add_systems(Startup, setup_app)
+    .add_systems(PostStartup, spawn_test_processes);
+
+    app.run();
 }
 
 fn setup_app(mut commands: Commands, mut clear_color: ResMut<ClearColor>) {
     commands.spawn(Camera2d::default());
     clear_color.0 = Color::BLACK;
+}
+
+fn spawn_test_processes(mut events: EventWriter<process::SpawnProcessEvent>) {
+    events.send_batch([
+        process::SpawnProcessEvent {
+            position: Vec2::new(-100.0, 0.0),
+        },
+        process::SpawnProcessEvent {
+            position: Vec2::new(80.0, 100.0),
+        },
+        process::SpawnProcessEvent {
+            position: Vec2::new(120.0, -100.0),
+        },
+    ]);
 }
---
+++ src/process.rs
@@ -0,0 +1,46 @@
+use bevy::{color::palettes::css::*, prelude::*};
+use bevy_prototype_lyon::{
+    prelude::*,
+    shapes::{RegularPolygon, RegularPolygonFeature},
+};
+
+#[derive(Event)]
+pub struct SpawnProcessEvent {
+    pub position: Vec2,
+}
+
+pub struct ProcessPlugin;
+
+impl Plugin for ProcessPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<SpawnProcessEvent>().add_systems(
+            Update,
+            spawn_processes.run_if(on_event::<SpawnProcessEvent>),
+        );
+    }
+}
+
+#[derive(Component)]
+struct Process;
+
+fn spawn_processes(mut commands: Commands, mut events: EventReader<SpawnProcessEvent>) {
+    let shape = RegularPolygon {
+        sides: 6,
+        center: Vec2::ZERO,
+        feature: RegularPolygonFeature::Radius(24.0),
+    };
+
+    for event in events.read() {
+        debug!("Spawning process at {:?}", event.position);
+        commands.spawn((
+            ShapeBundle {
+                path: GeometryBuilder::build_as(&shape),
+                transform: Transform::from_translation(event.position.extend(0.0)),
+                ..Default::default()
+            },
+            Fill::color(GREEN),
+            Stroke::new(DARK_GREEN, 4.0),
+            Process,
+        ));
+    }
+}
