5f1579709d3d8c1fc0a1438bc0d006ad89e4224f
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-12-01 18:29:30 -0600

    fresh bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,21 @@
-use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
-
-mod memory_bus;
-mod process;
-
-#[derive(Component)]
-struct MainCamera;
-
-#[derive(Resource)]
-struct MousePosition(Vec2);
+use bevy::{prelude::*, window::WindowResolution};
 
 fn main() {
+    println!("Hello, world!");
     App::new()
-        // Vendor plugins
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "RUN".to_owned(),
-                    mode: bevy::window::WindowMode::Windowed,
-                    resizable: true,
-                    ..Default::default()
-                }),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Oversimplified Computer Simulator".to_owned(),
+                resolution: WindowResolution::new(1152.0, 720.0),
                 ..Default::default()
             }),
-            ShapePlugin,
-        ))
-        // In-house plugins
-        .add_plugins((process::ProcessPlugin, memory_bus::MemoryBusPlugin))
-        .insert_resource(MousePosition(Vec2::ZERO))
-        .add_systems(Startup, setup)
-        .add_systems(Update, update_mouse_position)
+            ..Default::default()
+        }))
+        .add_systems(Startup, setup_app)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn((MainCamera, Camera2dBundle::default()));
-}
-
-fn update_mouse_position(
-    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
-    mut mouse_pos: ResMut<MousePosition>,
-    mut cursor_events: EventReader<CursorMoved>,
-) {
-    let (camera, camera_transform) = q_camera.single();
-
-    for event in cursor_events.read() {
-        mouse_pos.0 = camera
-            .viewport_to_world_2d(camera_transform, event.position)
-            .unwrap();
-    }
+fn setup_app(mut commands: Commands, mut clear_color: ResMut<ClearColor>) {
+    commands.spawn(Camera2d::default());
+    clear_color.0 = Color::BLACK;
 }
